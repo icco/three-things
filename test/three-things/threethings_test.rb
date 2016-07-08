3d92cded26fff05464f7c8de8e9d94c3761478fa
Nat Welch <nat@natwelch.com>
2016-07-08 13:52:09 -0700

    reorg test

--- test/three-things/threethings_test.rb
+++ test/three-things/threethings_test.rb
@@ -4,8 +4,4 @@ class ThreeThingsTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::ThreeThings::VERSION
   end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
