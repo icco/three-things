eb1383cd76b9f7e9e88337c51c0cc086068d7eff
Nat Welch <nat@natwelch.com>
2016-07-09 11:44:35 -0700

    tag everything with paths

--- lib/three-things/middleware.rb
+++ lib/three-things/middleware.rb
@@ -18,17 +18,19 @@ module ThreeThings
     end
 
     def call env
+      req = Rack::Request.new(env)
+
       # Request count
-      @statsd.increment path("request.count")
+      @statsd.increment path("request.count"), {path: req.path}
 
       # Reqest time
-      status, headers, body = @statsd.time path("request.time") do
+      status, headers, body = @statsd.time path("request.time"), {path: req.path} do
         @app.call env
       end
 
       # Request errors
       if status > 308
-        @statsd.increment path("request.error"), {status_code: status}
+        @statsd.increment path("request.error"), {status_code: status, path: req.path}
       end
 
       [status, headers, body]
